d70a7d821432bd4d4a07a99bcb3df975935f302e
Ishaan Dey <ishaan1013@gmail.com>
2023-08-01 00:49:59 -0400

    remove static exp

--- nextjs/app/api/worker/route.ts
+++ nextjs/app/api/worker/route.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { NextResponse } from "next/server"
 
+export const runtime = "edge"
+
 export async function POST(request: Request) {
   const body = await request.json()
 
